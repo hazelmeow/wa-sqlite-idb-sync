db41cc522a0452033a75a1cc6b4d7bafa6df464b
hazel <hazel@meows.zip>
2024-09-16 16:36:43 -0700

    output types

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
+  plugins: [
+    dts({
+      rollupTypes: true,
+    }),
+  ],
   resolve: {
     alias: {
       "wa-sqlite": resolve(__dirname, "./vendor/wa-sqlite"),
